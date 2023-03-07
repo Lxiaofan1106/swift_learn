9066ecfe4d2703c620aa394813cf2f8822a1c3d7
LYL <3399124939@qq.com>
2023-03-07 18:46:41 +0800

    首次提交

---
+++ learnSwift/learnSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  learnSwift
+//
+//  Created by LYL on 2023/3/7.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
